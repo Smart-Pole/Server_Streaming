0ac3e83dac8ad4eaf9b7d9c6b6b08756d1b292d0
NhanHuynhtv0104 <hieunhantv2002@gmail.com>
2024-08-05 20:25:10 +0700

    obs websocket controller add set size for source, get and set tranform of source

--- obs.bat
+++ obs.bat
@@ -1,5 +1,7 @@
 @echo off
-start /d "D:\APPDATA\OBS\obs-studio\bin\64bit" obs64.exe -m --websocket_port 3344 --websocket_password "123456"
-start /d "D:\APPDATA\OBS\obs-studio\bin\64bit" obs64.exe -m --websocket_port 5544 --websocket_password "123456"
+set obsPath="D:\APPDATA\OBS\obs-studio\bin\64bit"
+@REM set obsPath="D:\application\obs-studio\bin\64bit"
+start /d %obsPath% obs64.exe -m --websocket_port 3344 --websocket_password "123456" --minimize-to-tray
+start /d %obsPath% obs64.exe -m --websocket_port 5544 --websocket_password "123456" --minimize-to-tray
 
     
